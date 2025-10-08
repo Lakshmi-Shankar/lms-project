7490e16591a205c6dc1598afb1a5c94eea94f634
Shankar <lakshmi7708671565@gmail.com>
2025-10-08 10:36:08 +0530

    Latest commit

--- src/components/layout/Dashboard.jsx
+++ src/components/layout/Dashboard.jsx
@@ -1,10 +1,10 @@
 import Navbar from "../layout/NavBar";
 import Footer from "../layout/Footer";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar children={undefined} />
+      <Navbar />
       <main className="flex-1 p-6 bg-gray-100">{children}</main>
       <Footer />
     </div>
